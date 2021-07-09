334b911a0fa54b9ed2572a27c0e45b7130090ef3
mgfaundes <87110758+mgfaundes@users.noreply.github.com>
2021-07-09 03:08:09 -0400

    Inico con librerias
    Con el compañero se revisan las librerías dispuestas para comenzar, se definen funciones para abrir los archivos, además de explorar el formato de los datos

--- proyecto.py
+++ proyecto.py
@@ -1,2 +1,39 @@
-def funcion():
-    print("hola")
+import os
+import time
+import matplotlib.pyplot as plt
+archivo = None
+def bienvenida():
+    print("Bienvenido al organizador de datos COVID-19")
+    print("Tiene descargada la database (Y/N) \n")
+
+def abrirdatos():
+	aux=open("CasosNuevosSinSintomas.csv","r")
+	archivo=aux.readlines()
+	return archivo
+
+def abrirdata2():
+    aux = open("CasosNuevosSinSintomas_std.csv","r")
+    archivo2 = aux.readlines()
+    return archivo2
+
+def abrirdata3():
+    aux = open("CasosNuevosSinSintomas_T.csv","r")
+    archivo3 = aux.readlines()
+    return archivo3
+
+def mostrardatos(archivo):
+	for i in archivo:
+		i=i.split(",")
+		aux=""
+		for j in i:
+			aux=aux+j+"\t"
+		print(aux)
+
+
+
+plt.plot([1, 2, 3, 4])
+plt.ylabel("ejemplo")
+plt.show()
+
+archivo = abrirdatos()
+mostrardatos(archivo)
