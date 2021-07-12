59e2d662c93d079086e7edbbff5ab4ae131e6dc2
mgfaundes <87110758+mgfaundes@users.noreply.github.com>
2021-07-12 06:37:14 -0400

    Proyectofinal

--- proyecto.py
+++ proyecto.py
@@ -1,39 +1,52 @@
 import os
 import time
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+datos = None
 archivo = None
 def bienvenida():
     print("Bienvenido al organizador de datos COVID-19")
-    print("Tiene descargada la database (Y/N) \n")
 
 def abrirdatos():
-	aux=open("CasosNuevosSinSintomas.csv","r")
+	aux=open("CasosNuevosSinSintomas_T.csv","r")
 	archivo=aux.readlines()
-	return archivo
+	archivo = "".join(archivo)
+	archivo = archivo.replace(",",";")
+	datos = open("Database.csv", "w")
+	datos.write(archivo)
+	aux2 = open("Database.csv", "r")
+	datos = aux2.readlines()
+	return datos
 
-def abrirdata2():
-    aux = open("CasosNuevosSinSintomas_std.csv","r")
-    archivo2 = aux.readlines()
-    return archivo2
+def abrirdatos2():
+	aux=open("ResidenciasSanitarias_T.csv","r")
+	archivo=aux.readlines()
+	archivo = "".join(archivo)
+	archivo = archivo.replace(",",";")
+	datos2 = open("Database2.csv", "w")
+	datos2.write(archivo)
+	aux2 = open("Database2.csv", "r")
+	datos2 = aux2.readlines()
+	return datos2
 
-def abrirdata3():
-    aux = open("CasosNuevosSinSintomas_T.csv","r")
-    archivo3 = aux.readlines()
-    return archivo3
-
-def mostrardatos(archivo):
-	for i in archivo:
-		i=i.split(",")
+def mostrardatos():
+	for i in datos:
+		i=i.split(";")
 		aux=""
 		for j in i:
 			aux=aux+j+"\t"
 		print(aux)
 
+datos = abrirdatos()
+datos2 = abrirdatos2()
+mostrardatos()
 
 
+
+'''
 plt.plot([1, 2, 3, 4])
 plt.ylabel("ejemplo")
 plt.show()
+'''
 
-archivo = abrirdatos()
-mostrardatos(archivo)
